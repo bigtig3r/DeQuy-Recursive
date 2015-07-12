4134c14557d43b717cc123fe7651a26525bd8dfc
Dat NT (Mr.) <trandat.nguyentd@gmail.com>
2015-07-12 07:26:33 +0700

    De Quy - Recursive
    N! = ???

---
+++ DeQuy-Recursive/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeQuy_Recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input N : ");
+            double d = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("N! = {0}", Recursive(d));
+            Console.Read();
+
+        }
+
+        private static double Recursive(double n)
+        {
+            if (n > 0)
+                return n * Recursive(n - 1);
+            return 1;
+        }
+    }
+}
